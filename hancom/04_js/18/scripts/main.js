5c5592ff1146594c4c781e70ff8eb0ba8f986b32
hyoinkim <daepo333@gmail.com>
2026-07-08 16:37:44 +0900

    feat: 미니프로젝트임시로마무리

--- hancom/04_js/18/scripts/main.js
+++ hancom/04_js/18/scripts/main.js
@@ -13,11 +13,6 @@ toggle.addEventListener("click", () => {
 });
 
 
-// 투두 (등록, 삭제 : localStorage 사용 및 DOM 조작 연습)
-
-const todo = [];
-
-
 // 캐러셀 (앞으로, 뒤로, 자동, 마우스호버이벤트 : event, transition 연습)
 
 const track = document.querySelector(".carousel__track");
@@ -89,4 +84,102 @@ setInterval(() => {
   }
 }, 1000);
 
+
+const DUMMY = [
+    {
+        "이름" : "홍길동",
+        "나이" : 120,
+        "주소" : "서울",
+        "전화번호" : 4222,
+    },
+    {
+        "이름" : "김길동",
+        "나이" : 20,
+        "주소" : "대구",
+        "전화번호" : 1322,
+    },
+    {
+        "이름" : "박길동",
+        "나이" : 130,
+        "주소" : "대전",
+        "전화번호" : 3322,
+    },
+    {
+        "이름" : "홍길동",
+        "나이" : 100,
+        "주소" : "부산",
+        "전화번호" : 5522,
+    }
+];
+
+const DUMMY__HEADER = ["이름","나이","주소","전화번호"]
+
 // 테이블 (column 정렬 메서드 : 순회메서드 연습)
+
+const table = document.querySelector(".table");
+const row__num = DUMMY.length;
+const tr = document.createElement("tr");
+tr.classList.add("table-row")
+table.appendChild(tr);
+DUMMY__HEADER.forEach((el)=>{
+    const th = document.createElement("th");
+    th.classList.add("table-header");
+    th.textContent = el;
+    tr.appendChild(th);
+})
+
+DUMMY.forEach((el=>{
+    const tr2 = document.createElement("tr");
+    tr2.classList.add("table-row");
+    table.appendChild(tr2);
+
+    DUMMY__HEADER.forEach((key)=>{
+        const td = document.createElement("td");
+        td.classList.add("table-data");
+        td.textContent = el[key];
+        tr2.appendChild(td);
+    })
+}))
+
+
+// 투두 (등록, 삭제 : localStorage 사용 및 DOM 조작 연습)
+
+const todo__form = document.querySelector(".todo-form");
+const todo__input = document.querySelector(".todo-input");
+const todo__list = document.querySelector(".todo-list");
+
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
+// todos: string[]
+
+const save = ()=>{
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+const renderTodo = (text)=>{
+    const todo = document.createElement("li");
+    todo.textContent = text;
+
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "삭제";
+    delBtn.addEventListener("click", ()=>{
+        todos.splice(todos.indexOf(text), 1);
+        todo.remove();
+        save();
+    });
+
+    todo.appendChild(delBtn);
+    todo__list.appendChild(todo);
+}
+
+todos.forEach(renderTodo);
+
+todo__form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const text = todo__input.value.trim();
+    if(!text) return;
+
+    todos.push(text);
+    renderTodo(text);
+    save();
+    todo__input.value = "";
+})
